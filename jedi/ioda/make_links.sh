e4ecd4bc7359983982f6cc559bcdae13d9e0d6fa
Parallel Works app-run user <Jeffrey.S.Whitaker@mgmt-jeffreyswhitaker-stream1landda-00001.pw-noaa-us-east-2.pw.local>
2023-02-07 22:25:35 +0000

    updates to run on AWS

--- jedi/ioda/make_links.sh
+++ jedi/ioda/make_links.sh
@@ -1,11 +1,11 @@
-#!/bin/ksh 
+#!/bin/bash 
 
 if [ $# == 1 ]; then 
         echo "setting jedi path to input $1"
         jedi_path=$1
 else 
-        jedi_path="/scratch2/BMC/gsienkf/UFS-RNR/UFS-RNR-stack/external/ioda-bundle/"
+        jedi_path="/scratch2/BMC/gsienkf/UFS-RNR/UFS-RNR-stack/external/ioda-bundle/iodaconv/src/land"
 fi 
 
-ln -s ${jedi_path}/iodaconv/src/land/imsfv3_scf2ioda.py . 
+/bin/cp -f ${jedi_path}/imsfv3_scf2ioda.py . 
 
