4d2ae92f295c12cdf5df52e70fc8aa981dfda4a1
Parallel Works app-run user <Jeffrey.S.Whitaker@mgmt-jeffreyswhitaker-stream1landda-00001.pw-noaa-us-east-2.pw.local>
2023-02-09 02:23:42 +0000

    remove

--- run_apply_incr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. /etc/profile.d/z10_spack_environment.sh
-export LD_LIBRARY_PATH="/opt/view/lib:$LD_LIBRARY_PATH"
-echo "mpirun -np 6 --oversubscribe ${INCR_EXECDIR}/apply_incr ${LOGDIR}/apply_incr.log"
-mpirun -np 6 --oversubscribe ${INCR_EXECDIR}/apply_incr ${LOGDIR}/apply_incr.log
-echo "done running apply_incr"
