abfe00e0c2e3e9c0f23ccae094f39e10dff53eee
Parallel Works Guest Account <Jeffrey.S.Whitaker@mgmt-philippegion-stream1-00007.pw-noaa-us-east-2.pw.local>
2023-02-15 21:29:53 +0000

    use local ObsSpace.yaml

--- run_iodav2tov3.sh
+++ run_iodav2tov3.sh
@@ -3,5 +3,5 @@
 IODA_V2_FILE=$1
 IODA_V3_FILE=$2
 export LD_LIBRARY_PATH=/lustre/${USER}/fv3-jedi/build/lib:$JEDI_LD_LIBRARY_PATH
-mpirun -np 1 --oversubscribe /lustre/${USER}/soca-science/build/bin/ioda-upgrade-v2-to-v3.x $IODA_V2_FILE $IODA_V3_FILE /contrib/role.ca-ufs-rnr/soca-science/bundle/ioda/share/ioda/yaml/validation/ObsSpace.yaml
+mpirun -np 1 --oversubscribe /lustre/${USER}/soca-science/build/bin/ioda-upgrade-v2-to-v3.x $IODA_V2_FILE $IODA_V3_FILE ${LANDDADIR}/ObsSpace.yaml
 echo "done running ioda v2 to v3 converter"
