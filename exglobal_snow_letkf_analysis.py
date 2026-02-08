db26e445268d0a0e263fed2961019cbc38a46add
tsga <zacctsega@yahoo.com>
2026-02-08 11:21:41 +0000

    use letkf instead of ens

--- exglobal_snow_letkf_analysis.py
+++ exglobal_snow_letkf_analysis.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # exglobal_snow_letkf_analysis.py
 # (adapted from exglobal_snowens_analysis.py)
-# This script creates an SnowEnsAnalysis class instance from snow_letkf_analysis,
-# which will process snow observations, run a LETKF analysis, and add increments 
+# This script creates an SnowLetkfAnalysis class instance from snow_letkf_analysis,
+# which will process snow observations, run a LETKF analysis, and add increments
 # to background ensmbles to create an ensemble of snow analyses
 import os
 
 from wxflow import Logger, cast_strdict_as_dtypedict
-from pygfs.task.snow_letkf_analysis import SnowEnsAnalysis
+from pygfs.task.snow_letkf_analysis import SnowLetkfAnalysis
 
 # Initialize root logger
 logger = Logger(level=os.environ.get("LOGGING_LEVEL", "DEBUG"), colored_log=True)
@@ -19,30 +19,30 @@ if __name__ == '__main__':
     config = cast_strdict_as_dtypedict(os.environ)
 
     # Instantiate the snow ensemble analysis task
-    snow_ens_anl = SnowEnsAnalysis(config)
+    snow_letkf_anl = SnowLetkfAnalysis(config)
 
     # Initialize JEDI 2DVar snow analysis
-    snow_ens_anl.initialize()
+    snow_letkf_anl.initialize()
 
     # stage ensemble mean backgrounds ??why?
 
     # Process SNOCVR and SNOMAD (if applicable)
-    if snow_ens_anl.task_config.DO_SNOCVR_SNOMAD:
-        snow_ens_anl.prepare_SNOCVR_SNOMAD()
+    if snow_letkf_anl.task_config.DO_SNOCVR_SNOMAD:
+        snow_letkf_anl.prepare_SNOCVR_SNOMAD()
 
     # Process IMS snow cover (if applicable)
-    if snow_ens_anl.task_config.DO_IMS_SCF:
-        snow_ens_anl.execute('scf_to_ioda')
+    if snow_letkf_anl.task_config.DO_IMS_SCF:
+        snow_letkf_anl.execute('scf_to_ioda')
 
     # Process GHCN (if applicable)
-    if snow_ens_anl.task_config.DO_GHCN:
-        snow_ens_anl.prepare_GHCN()
+    if snow_letkf_anl.task_config.DO_GHCN:
+        snow_letkf_anl.prepare_GHCN()
 
     # Execute JEDI snow analysis
-    snow_ens_anl.execute('snowensanlletkf')
+    snow_letkf_anl.execute('snowletkfanl')
 
     # Add increments
-    snow_ens_anl.add_increments()
+    snow_letkf_anl.add_increments()
 
     # Finalize JEDI snow analysis
-    snow_ens_anl.finalize()
+    snow_letkf_anl.finalize()
